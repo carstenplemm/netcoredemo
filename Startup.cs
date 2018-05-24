992e73c3958c7279016d43271bd5bf4cc2c4742b
Carsten Lemm <clemm@Carstens-MBP.fritz.box>
2018-05-24 10:47:13 +0200

    initial version

---
+++ Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace dotnet
+{
+    class Startup
+    {
+	public Startup(IHostingEnvironment env)
+        {
+	   	
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logerFactory)
+        {
+	   app.Run(async (context) =>
+           {
+	      await context.Response.WriteAsync("Hello World.");
+           });
+        }
+    }
+}
